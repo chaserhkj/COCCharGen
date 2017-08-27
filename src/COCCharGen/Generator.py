0f06a536d62220f94882f9da82f80cbd08062213
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2017-08-27 02:55:59 -0400

    Changed Generator default action to accept

--- src/COCCharGen/Generator.py
+++ src/COCCharGen/Generator.py
@@ -1,20 +1,20 @@
 class Generator():
     """Virtual class for Generators."""
     def generate_basic_info(self, char):
-        raise NotImplemented
+        pass
     def generate_characteristics(self, char):
-        raise NotImplemented
+        pass
     def generate_status(self, char):
-        raise NotImplemented
+        pass
     def generate_combact_stat(self, char):
-        raise NotImplemented
+        pass
     def generate_skills(self, char):
-        raise NotImplemented
+        pass
     def generate_weapons(self, char):
-        raise NotImplemented
+        pass
     def generate_backstory(self, char):
-        raise NotImplemented
+        pass
     def generate_inventory(self, char):
-        raise NotImplemented
+        pass
     def generate_financial_status(self, char):
-        raise NotImplemented
+        pass
